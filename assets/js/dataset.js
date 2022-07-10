fb7c27f97cf83bd589514fd65ced31e4edcc4265
MEMEMEP <73705410+MEMEMEP@users.noreply.github.com>
2022-07-10 22:58:52 +0700

    database

--- assets/js/dataset.js
+++ assets/js/dataset.js
@@ -1,24 +1,209 @@
-exports.hocBong = [
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' }
+exports.Register = [
+    {
+        idNumber: 1,
+        name: 'Hoang Thi Thuy',
+        dob: '01/10/2000',
+        gender: 'female',
+        email:'hoangthithuy@gmail.com',
+        major: 'Digital Marketing',
+       
+       },
+       {
+        idNumber: 2,
+        name: 'Vu Van Truong',
+        dob: '29/01/2000',
+        gender: 'male',
+        email: 'vuvantruong@gmail.com',
+        major: 'Artificial Intelligence',
+       
+       },
+       {
+        idNumber: 3,
+        name: 'Nguyen Thuy Vy',
+        dob: '14/08/2000',
+        gender: 'female',
+        email: 'nguyenthuyvy@gmail.com',
+        major: 'Digital Art & Design',
+       
+       },
+       {
+        idNumber: 4,
+        name: 'Do Quynh Trang',
+        dob: '18/09/2000',
+        gender: 'female',
+        email: 'doquynhtrang@gmail.com',
+        major: 'Ki thuat phan mem',
+       
+       },
+       {
+        idNumber: 5,
+        name: 'Nguyen Nhat Linh',
+        dob: '06/07/2000',
+        gender: 'male',
+        email: 'nguyennhatlinh@gmail.com',
+        major: 'Kinh doanh quoc te',
+       
+       },
+       {
+        idNumber: 6,
+        name: 'Ha Hoang Hai',
+        dob: '30/04/2000',
+        gender: 'male',
+        email: 'hahoanghai@gmail.com',
+        major: 'Ngon ngu Nhat',
+       
+       }
 
 
 ];
 
 
-exports.name= [
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' }
+exports.School= [
+    {
+        schoolId: '',
+        schoolName: ''
+        
+        },
 
+
+];
+
+exports.Major= [
+    {
+        majorId:'',
+        majorName:''
+        
+        },
+
+
+];
+
+exports.Subject= [
+    {
+        subjectId:'',
+        subjectName:''
+        
+        },
+
+
+];
+
+exports.Profile= [
+    {
+        studentId: 1,
+        majorId: '',
+        name: 'Bui Huu Tu',
+        dob: '09/04/2001',
+        gender: 'male',
+        idNumber: '',
+        
+    },
+    {
+        studentId: 2,
+        majorId: '',
+        name: 'Tran Viet Hoang',
+        dob: '23/08/2000',
+        gender: 'male',
+        idNumber: '',
+        
+    },
+    {
+        studentId: 3,
+        majorId: '',
+        name: 'Nguyen Van Anh',
+        dob: '15/06/2000',
+        gender: 'female',
+        idNumber: '',
+        
+    },
+    {
+        studentId: 4,
+        majorId: '',
+        name: 'Nguyen Tuan Dat',
+        dob: '03/12/2000',
+        gender: 'male',
+        idNumber: '',
+        
+    },
+    {
+        studentId: 5,
+        majorId: '',
+        name: 'Le Anh Minh',
+        dob: '13/5/2001',
+        gender: 'male',
+        idNumber: '',
+        
+    }
+
+
+];
+
+exports.Exam= [
+    {
+        examId
+        profileId
+        subjectId
+        mark
+              
+        },
+
+
+];
+
+exports.Result= [
+    {
+        examId: 1
+        profileId
+        subjectId
+        name: '',
+        idNumber: '',
+        mark
+        bonusMark
+        totalMark
+        scholarship
+                
+        
+        },
+
+
+];
+
+exports.Admin= [
+    {
+        adminId: 1,
+        name: 'ThanhNH3',
+        username: 'thanhnh3',
+        password: '12345678',
+                
+    },
+    {
+        adminId: 2,
+        name: 'HungTD14',
+        username: 'hungtd14',
+        password: '12345678',
+                
+    },
+    {
+        adminId: 3,
+        name: 'DuongBN5',
+        username: 'duongbn5',
+        password: '12345678',
+                
+    },
+    {
+        adminId: 4,
+        name: 'HangNG12',
+        username: 'hangng12',
+        password: '12345678',
+                
+    },
+    {
+        adminId: 5,
+        name: 'MinhTH7',
+        username: 'minhth7',
+        password: '12345678',
+                
+    },
+    
 
 ];
