594df05d7525992e07c132a0a7cbb1954784d203
thanglv <vietthang1118@gmail.com>
2022-07-11 10:38:28 +0700

    ref: create homepage, login, forgetPassword

--- assets/js/dataset.js
+++ assets/js/dataset.js
@@ -1,24 +1,21 @@
 exports.hocBong = [
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' }
-
-
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
 ];
 
-
-exports.name= [
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' },
-    { id: 1, content: 'bean1221212' }
-
-
+exports.hocBong = [
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
+  { id: 1, content: "bean1221212" },
 ];
