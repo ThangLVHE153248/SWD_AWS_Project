c91e783f9ef59216d353fe8a659a4cbc194d17f6
MEMEMEP <73705410+MEMEMEP@users.noreply.github.com>
2022-07-11 10:32:30 +0700

    data

--- assets/js/dataset.js
+++ assets/js/dataset.js
@@ -1,137 +1,105 @@
 exports.Register = [
     {
-        idNumber: 1,
         name: 'Hoang Thi Thuy',
         dob: '01/10/2000',
-        gender: 'female',
         email:'hoangthithuy@gmail.com',
-        major: 'Digital Marketing',
+        phone: '123-456-654'
        
        },
        {
-        idNumber: 2,
         name: 'Vu Van Truong',
         dob: '29/01/2000',
-        gender: 'male',
         email: 'vuvantruong@gmail.com',
-        major: 'Artificial Intelligence',
+        phone: '123-456-212'
        
        },
        {
-        idNumber: 3,
         name: 'Nguyen Thuy Vy',
         dob: '14/08/2000',
-        gender: 'female',
         email: 'nguyenthuyvy@gmail.com',
-        major: 'Digital Art & Design',
+        phone: '123-456-135'
        
        },
        {
-        idNumber: 4,
         name: 'Do Quynh Trang',
         dob: '18/09/2000',
-        gender: 'female',
         email: 'doquynhtrang@gmail.com',
-        major: 'Ki thuat phan mem',
+        phone: '123-456-425'
        
        },
        {
-        idNumber: 5,
         name: 'Nguyen Nhat Linh',
         dob: '06/07/2000',
-        gender: 'male',
         email: 'nguyennhatlinh@gmail.com',
-        major: 'Kinh doanh quoc te',
+        phone: '123-456-984'
        
        },
        {
-        idNumber: 6,
         name: 'Ha Hoang Hai',
         dob: '30/04/2000',
-        gender: 'male',
         email: 'hahoanghai@gmail.com',
-        major: 'Ngon ngu Nhat',
-       
+        phone: '123-456-150'
        }
 
 
 ];
 
 
-exports.School= [
-    {
-        schoolId: '',
-        schoolName: ''
-        
-        },
-
-
-];
-
-exports.Major= [
-    {
-        majorId:'',
-        majorName:''
-        
-        },
-
-
-];
-
-exports.Subject= [
-    {
-        subjectId:'',
-        subjectName:''
-        
-        },
-
-
-];
 
 exports.Profile= [
     {
-        studentId: 1,
-        majorId: '',
+        profileId: 1,
         name: 'Bui Huu Tu',
         dob: '09/04/2001',
-        gender: 'male',
-        idNumber: '',
+        gender: 'M',
+        idNumber: '89h3hubfeui',
+        email: 'buihuutu@gmail.com',
+        phone:'123-456-515',
+        school: 'THPT ABC'
         
     },
     {
-        studentId: 2,
-        majorId: '',
+        profileId: 2,
         name: 'Tran Viet Hoang',
         dob: '23/08/2000',
-        gender: 'male',
-        idNumber: '',
+        gender: 'M',
+        idNumber: 'uih3498fh3h',
+        email: 'tranviethoang@gmail.com',
+        phone:'123-456-420',
+        school: 'THPT ABC'
         
     },
     {
-        studentId: 3,
-        majorId: '',
+        profileId: 3,
         name: 'Nguyen Van Anh',
         dob: '15/06/2000',
-        gender: 'female',
-        idNumber: '',
+        gender: 'F',
+        idNumber: '3g7f8giu3bui',
+        email: 'nguyenvananh@gmail.com',
+        phone:'123-456-210',
+        school: 'THPT ABC'
         
     },
     {
-        studentId: 4,
-        majorId: '',
+        profileId: 4,
         name: 'Nguyen Tuan Dat',
         dob: '03/12/2000',
-        gender: 'male',
-        idNumber: '',
+        gender: 'M',
+        idNumber: '98389fh8934',
+        email: 'nguyentuandat@gmail.com',
+        phone:'123-456-782',
+        school: 'THPT ABC'
         
     },
     {
-        studentId: 5,
-        majorId: '',
+        profileId: 5,
         name: 'Le Anh Minh',
         dob: '13/5/2001',
-        gender: 'male',
-        idNumber: '',
+        gender: 'M',
+        idNumber: 'aqe38hgu2o',
+        email: 'leanhminh@gmail.com',
+        phone:'123-456-128',
+        school: 'THPT ABC'
         
     }
 
@@ -152,18 +120,40 @@ exports.Exam= [
 
 exports.Result= [
     {
-        examId: 1
-        profileId
-        subjectId
-        name: '',
-        idNumber: '',
-        mark
-        bonusMark
-        totalMark
-        scholarship
-                
-        
-        },
+        profileId: 1,
+        name: 'Bui Huu Tu',
+        idNumber: '89h3hubfeui',
+        totalMark: ,
+        scholarship:
+    },
+    {
+        profileId: 2,
+        name: 'Tran Viet Hoang',
+        idNumber: 'uih3498fh3h',
+        totalMark: ,
+        scholarship:
+    },
+    {
+        profileId: 3,
+        name: 'Nguyen Van Anh',
+        idNumber: '3g7f8giu3bui',
+        totalMark: ,
+        scholarship:
+    },
+    {
+        profileId: 4,
+        name: 'Nguyen Tuan Dat',
+        idNumber: '98389fh8934',
+        totalMark: ,
+        scholarship:
+    },
+    {
+        profileId: 5,
+        name: 'Le Anh Minh',
+        idNumber: 'aqe38hgu2o',
+        totalMark: ,
+        scholarship:
+    },
 
 
 ];
